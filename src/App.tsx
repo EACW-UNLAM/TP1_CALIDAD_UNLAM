8ff856ad3c6f91dcc886cc931fdb8e1260d1dce6
Alejo Burnowicz <Alejoburnowicz@yahoo.com.ar>
2023-04-09 12:49:26 -0300

    sefinish

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FormEvent, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [lado1, setLado1] = useState<number | null>(null);
+  const [lado2, setLado2] = useState<number | null>(null);
+  const [lado3, setLado3] = useState<number | null>(null);
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (lado1 === null || lado2 === null || lado3 === null) {
+      alert('Completa los campos para poder calcular el triangulo');
+      return;
+    }
+    if (lado1 <= 0 || lado2 <= 0 || lado3 <= 0) {
+      alert('Ningun lado del triangulo puede ser 0 o menor que 0 ');
+      return;
+    }
+
+    if (lado1 === lado2 && lado2 === lado3) {
+      alert('El triangulo es equilátero');
+      return;
+    }
+
+    if (lado1 === lado2 || lado2 === lado3 || lado1 === lado3) {
+      alert('Triângulo Isósceles');
+      return;
+    }
+
+    alert('Triângulo Escaleno');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <form action=''>
+          <label htmlFor='lado1'>Lado 1</label>
+          <input
+            type='number'
+            name='lado1'
+            id='lado1'
+            min='0'
+            onChange={(e) => setLado1(Number(e.target.value))}
+          />
+          <br />
+          <label htmlFor='lado2'>Lado 2</label>
+          <input
+            type='number'
+            name='lado2'
+            id='lado2'
+            min='0'
+            onChange={(e) => setLado2(Number(e.target.value))}
+          />
+          <br />
+          <label htmlFor='lado3'>Lado 3</label>
+          <input
+            type='number'
+            name='lado3'
+            id='lado3'
+            min='0'
+            onChange={(e) => setLado3(Number(e.target.value))}
+          />
+          <br />
+          <input
+            type='submit'
+            value='Calcular'
+            onClick={(e) => handleSubmit(e)}
+          ></input>
+        </form>
       </header>
     </div>
   );
